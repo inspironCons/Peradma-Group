2617fde3dace1e6daee2226ab2b025c7aecb0185
inspironCons <dodon.consult@gmail.com>
2019-12-22 08:25:48 +0700

    update git ignore

---
+++ configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+    class GConfig{
+        var $_site_url = "http://paradma.kensa/";
+		var $_document_root = "C:/xampp/htdocs/Kensa-Project/";
+		var $_host_name = "localhost";
+		var $_database_name = "paradma";
+		var $_database_user = "root";
+		var $_database_password = "";
+		var $_table_prefix = "tbl_";
+		var $_web_name = "Paradma Group";
+		var $_title = "Paradma";
+		var $_post_page = 10;
+		var $_Book_page  = 10;
+	}
+	
+	
+?>
